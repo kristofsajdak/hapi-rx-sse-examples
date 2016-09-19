2dd0d3256d462636440b5d483fa65e20b441aa15
kristofsajdak <kristof.sajdak@gmail.com>
2016-09-19 09:51:06 +0200

    removed duplicate code block from publish.js

--- kafka-sse-filter/publish.js
+++ kafka-sse-filter/publish.js
@@ -6,23 +6,6 @@ const args = process.argv;
 const producer = kafkaSSEFilter.createProducer();
 
 producer.init()
-    .then(()=> send(producer, args[2], args[3]))
+    .then(()=> kafkaSSEFilter.send(producer, args[2], args[3]))
     .then(()=> producer.end())
     .then(()=> process.exit());
-
-function send(producer, type, title) {
-    return producer.send({
-        topic: 'all',
-        partition: 0,
-        message: {
-            key: `${type}.insert`,
-            value: JSON.stringify({
-                id: uuid.v4(),
-                type,
-                attributes: {
-                    title
-                }
-            })
-        }
-    });
-}
